5ace9b1f1319686db2ae491e42d9e841950078d1
MuchlasS <muchlas148@gmail.com>
2020-03-12 02:06:23 +0700

    update navbar: addnew featured 'Akun' and Search Bar

--- components/content/navside.vue
+++ components/content/navside.vue
@@ -7,16 +7,17 @@
             <template slot="title">Menu</template>
             <el-menu-item index="2-1">item one</el-menu-item>
             <el-menu-item index="2-2">item two</el-menu-item>
-            <el-menu-item index="2-3">item three</el-menu-item>
+            <el-menu-item index="2-3">item three</el-menu-item><hr>
             <el-submenu index="2-4">
-              <template slot="title">item four</template>
-              <el-menu-item index="2-4-1">item one</el-menu-item>
-              <el-menu-item index="2-4-2">item two</el-menu-item>
-              <el-menu-item index="2-4-3">item three</el-menu-item>
+              <template slot="title">Akun</template>
+              <el-menu-item index="2-4-1">Profil</el-menu-item>
+              <el-menu-item index="2-4-2">Keluar</el-menu-item>
             </el-submenu>
           </el-submenu>
-          <el-menu-item index="3" disabled>Info</el-menu-item>
-          <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">Orders</a></el-menu-item>
+          <el-menu-item index="3" class="el-icon-search">
+            Search
+            <input style="border: none; border-radius: 3px;" type="text">
+          </el-menu-item>
         </el-menu>
     </div>
 </template>
